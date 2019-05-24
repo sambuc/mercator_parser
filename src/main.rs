0eaa7839cf7f5a455c0f7adc302ace9eaae7c606
Lionel Sambuc <lionel.sambuc@epfl.ch>
2019-05-24 14:15:42 +0200

    Add timing info & use log for output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate measure_time;
+
 extern crate parser;
 
 use parser::queries;
@@ -5,11 +8,17 @@ use parser::queries;
 use std::io;
 
 fn main() {
+    // If RUST_LOG is unset, set it to INFO, otherwise keep it as-is.
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    pretty_env_logger::init();
+
     //let parser = queries::FiltersParser::new();
     let parser = queries::QueryParser::new();
 
     loop {
-        println!("\n> Expression to parse (type `quit` to exit): ");
+        info!("Expression to parse (type `quit` to exit): ");
 
         let mut input = String::new();
 
@@ -23,6 +32,13 @@ fn main() {
         if input.trim().eq_ignore_ascii_case("quit") {
             break;
         }
-        println!("\n> Tree: \n{:?}", parser.parse(input.as_str()));
+
+        let input = input.as_str();
+        let mut out;
+        {
+            debug_time!("Parsing");
+            out = parser.parse(input);
+        }
+        info!("Tree: \n{:?}", out);
     }
 }
